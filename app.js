c633e7a2bdfed92c43146e9f9ef8d798715e578c
Harsh Sethi <103830249+harsh-sethi@users.noreply.github.com>
2024-06-08 19:24:32 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+
+  $(".fa-bars").click(function(){
+    $(this).toggleClass("fa-times");
+    $("nav").toggleClass("nav-active");
+  });
+
+  $(window).on("load scroll", function(){
+    $(".fa-bars").removeClass("fa-times");
+    $("nav").removeClass("nav-active");
+  })
+
+  $(".count").each(function () {
+    var $this = $(this),
+      countTo = $this.attr("data-count");
+    $({ countNum: $this.text() }).animate(
+      {
+        countNum: countTo,
+      },
+      {
+        duration: 1750,
+        step: function () {
+          $this.text(Math.floor(this.countNum));
+        },
+        complete: function () {
+          $this.text(this.countNum + "+");
+        },
+      }
+    );
+  });
+
+  $(".project").magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: {
+      enabled: true,
+    }
+  })
+});
